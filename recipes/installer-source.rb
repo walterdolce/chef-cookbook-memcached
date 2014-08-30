d4a16ce2191e5261f387945f11bd78186d758b0d
Walter Dolce <walterdolce@gmail.com>
2014-08-30 15:20:40 +0100

    Add prereqs specs & commands for installer-source
    - Added prerequisite commands execution in order to ensure
      that Chef will be able to download & install memcache correclty.
      Current status is to check that make, yum, grep and tar are
      present on the system and that related commands run without
      errors.

--- recipes/installer-source.rb
+++ recipes/installer-source.rb
@@ -1,7 +1,42 @@
 
+execute 'check yum executable existence for later prerequisites checks' do 
+    command 'which yum'
+end
 
-remote_file 'download memcached source package file' do 
+execute 'check grep executable existence for later prerequisites checks' do 
+    command 'which grep'
+end
+
+execute 'check libevent existence on the system' do 
+    command 'yum list installed | grep libevent'
+end
+
+execute 'check tar executable existence to untar downloaded package' do 
+    command 'which tar'
+end
+
+execute 'check make executable existence to install downloaded package' do 
+    command 'which make'
+end
+
+remote_file 'download memcached package file' do 
     source node['memcached']['installer-source']['source-url']
     action :create
     path "#{node['memcached']['installer-source']['download-path']}#{node['memcached']['installer-source']['download-filename']}"
+end
+
+execute 'untar downloaded package' do 
+    command "tar -zxvf #{node['memcached']['installer-source']['download-path']}#{node['memcached']['installer-source']['download-filename']}"
+end
+
+execute 'configure installation path for downloaded package' do 
+    command "./configure --prefix=#{node['memcached']['installer-source']['installation-path']}memcached"
+end
+
+execute 'run make && make test before real installation' do 
+    command 'make && make test'
+end
+
+execute 'run make install to install downloaded packege' do 
+    command 'make install'
 end
